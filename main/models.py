e4eb5cbcf67b9179bf600c34c171d58b65f7deb9
franchuk <xcorter@mail.ru>
2014-11-29 15:23:52 +0600

    добавил урлов

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# class Quest(models.Model):
+#     name = models.CharField(max_length=255)
+#
+# class Booking(models.Model):
+#     quest = models.ForeignKey(Quest)
+
+
+class GiftCardOrder(models.Model):
+    phone = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+
+# class QuestOrder(models.Model)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class IndexView(generic.TemplateView):
+    template_name = 'main/index.html'
+
+
+class ContactView(generic.TemplateView):
+    template_name = 'main/contact.html'
+
+
+class RulesView(generic.TemplateView):
+    template_name = 'main/rules.html'
