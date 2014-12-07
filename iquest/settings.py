70af2d6f953c86f79e9662b15cfa768dbba34162
franchuk <xcorter@mail.ru>
2014-12-07 15:40:32 +0600

    убрал ганикорн

--- iquest/settings.py
+++ iquest/settings.py
@@ -33,8 +33,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
-    'gunicorn'
+    'main'
 )
 
 MIDDLEWARE_CLASSES = (
