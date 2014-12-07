e9a82e594471a6c77ad55d4e9f24df0383a0cc15
franchuk <xcorter@mail.ru>
2014-12-07 15:39:12 +0600

    добавил гинакорн

--- iquest/settings.py
+++ iquest/settings.py
@@ -33,7 +33,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'gunicorn'
 )
 
 MIDDLEWARE_CLASSES = (
