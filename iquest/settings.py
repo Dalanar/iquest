7fa3129b8d10fe098e17f0e9543b4537ed026b2f
franchuk <xcorter@mail.ru>
2014-12-07 15:09:04 +0600

    static root добавил

--- iquest/settings.py
+++ iquest/settings.py
@@ -99,3 +99,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+_PATH = os.path.abspath(os.path.dirname(__file__))
+
+STATIC_ROOT = os.path.join(_PATH, 'static')
