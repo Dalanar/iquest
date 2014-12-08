1e1d965627e46ce2bdc4dcbe6f5b6957c35f1da8
franchuk <xcorter@mail.ru>
2014-12-09 01:20:50 +0600

    делаю страницу квестов

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -28,6 +28,60 @@ $(document).ready(function(){
         });
     });
 
+   	$('.gallery .main-image > a').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true
+	});
+
+    $('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		}
+	});
+
     $.datepicker.setDefaults($.extend($.datepicker.regional["ru"]));
-    $("#datepicker").datepicker();
+    $("#datepicker").datepicker({
+        minDate: 0,
+        maxDate: "+1M +10D",
+        onSelect: function (date) {
+            $('.order-time-menu').dialog("open");
+            $('[name=date]').val(date);
+        }
+    });
+    $("#datepicker").find('.ui-datepicker-current-day').removeClass("ui-datepicker-current-day");
+
+    $('.order-time-menu').dialog({
+        autoOpen: false,
+        title: 'Выберите время',
+        width: 183,
+        modal: true
+    });
+
+    $('.time-money .row').click(function(){
+        var time = $(this).find('div:first').data('time');
+        $('[name=time]').val(time);
+        $('.order-time-menu').dialog('close');
+        checkConfirm();
+    });
+
+    $(".select-quest .order-quest").click(function(){
+        $('.select-quest .order-quest.current').removeClass('current');
+        $(this).addClass('current');
+        $('[name=quest]').val($(this).data('id'));
+        checkConfirm();
+    });
+
+    function checkConfirm() {
+        $('.confirm .quest-name').text($('.select-quest .order-quest.current').text())
+        $('.confirm .quest-date').text($('[input=date]').val());
+        $('.confirm .quest-time').text($('[input=time]').val());
+    }
 });
+
+checkConfirm();
