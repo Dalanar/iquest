98b74cb090916d3596f2ad9fb3dc8ca1c05d0fe6
franchuk <xcorter@mail.ru>
2015-04-22 10:17:39 +0700

    исправил баг

--- main/views.py
+++ main/views.py
@@ -192,7 +192,7 @@ def data_validate(name, email, phone):
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        ip = x_forwarded_for.split
+        ip = x_forwarded_for
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
