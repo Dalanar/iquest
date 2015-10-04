9dfe4e64029b6d834b725961cbfb3f3e28ec37eb
franchuk <xcorter@mail.ru>
2015-10-05 00:20:14 +0700

    изменил расписание

--- main/schedule/hospital.py
+++ main/schedule/hospital.py
@@ -6,53 +6,43 @@ from main.schedule.costs import *
 # понедельник, вторник, среда, четверг
 hospital_weekday = {
     0: {
-        "time": "12:00",
+        "time": "10:30",
         "cost": low_cost,
     },
     1: {
-        "time": "13:20",
+        "time": "12:30",
         "cost": low_cost,
         "order": 1
     },
     2: {
-        "time": "14:40",
+        "time": "14:30",
         "cost": low_cost,
         "order": 2
     },
     3: {
-        "time": "16:00",
+        "time": "16:30",
         "cost": middle_cost,
         "order": 3
     },
     4: {
-        "time": "17:20",
-        "cost": middle_cost,
+        "time": "18:30",
+        "cost": high_cost,
         "order": 4
     },
     5: {
-        "time": "18:40",
+        "time": "20:30",
         "cost": high_cost,
         "order": 5
     },
     6: {
-        "time": "20:00",
+        "time": "22:30",
         "cost": high_cost,
         "order": 6
     },
     7: {
-        "time": "21:20",
+        "time": "00:30",
         "cost": high_cost,
         "order": 7
-    },
-    8: {
-        "time": "22:40",
-        "cost": high_cost,
-        "order": 8
-    },
-    9: {
-        "time": "00:00",
-        "cost": high_cost,
-        "order": 9
     }
 }
 
@@ -119,59 +109,44 @@ hospital_weekday = {
 # суббота или выходной, после которого выходной
 hospital_weekend = {
     0: {
-        "time": "10:40",
+        "time": "10:30",
         "cost": middle_cost,
         "order": 0
     },
     1: {
-        "time": "12:00",
+        "time": "12:30",
         "cost": middle_cost,
         "order": 1
     },
     2: {
-        "time": "13:20",
+        "time": "14:30",
         "cost": high_cost,
         "order": 2
     },
     3: {
-        "time": "14:40",
+        "time": "16:30",
         "cost": high_cost,
         "order": 3
     },
     4: {
-        "time": "16:00",
+        "time": "18:30",
         "cost": high_cost,
         "order": 4
     },
     5: {
-        "time": "17:20",
+        "time": "20:30",
         "cost": high_cost,
         "order": 5
     },
     6: {
-        "time": "18:40",
+        "time": "22:30",
         "cost": high_cost,
         "order": 6
     },
     7: {
-        "time": "20:00",
+        "time": "00:30",
         "cost": high_cost,
         "order": 7
-    },
-    8: {
-        "time": "21:20",
-        "cost": high_cost,
-        "order": 8
-    },
-    9: {
-        "time": "22:40",
-        "cost": high_cost,
-        "order": 9
-    },
-    10: {
-        "time": "00:00",
-        "cost": high_cost,
-        "order": 10
     }
 }
 
