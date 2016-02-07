421e027c48c025a9396e1fd687252d03b86eeae1
franchuk <xcorter@mail.ru>
2016-02-07 14:22:41 +0600

    изменил расписание

--- main/schedule/genius.py
+++ main/schedule/genius.py
@@ -9,34 +9,44 @@ genius_weekday = {
         "order": 3
     },
     1: {
-        "time": "12:00",
+        "time": "11:30",
         "cost": low_cost,
         "order": 4
     },
     2: {
-        "time": "14:00",
+        "time": "13:00",
         "cost": middle_cost,
         "order": 5
     },
     3: {
-        "time": "16:00",
+        "time": "14:30",
         "cost": middle_cost,
         "order": 6
     },
     4: {
-        "time": "18:00",
-        "cost": high_cost,
+        "time": "16:00",
+        "cost": middle_cost,
         "order": 7
     },
     5: {
-        "time": "20:00",
+        "time": "17:30",
         "cost": high_cost,
         "order": 7
     },
     6: {
-        "time": "22:00",
+        "time": "19:00",
         "cost": high_cost,
         "order": 7
+    },
+    7: {
+        "time": "20:30",
+        "cost": high_cost,
+        "order": 7
+    },
+    8: {
+        "time": "22:00",
+        "cost": high_cost,
+        "order": 8
     }
 }
 
@@ -44,36 +54,46 @@ genius_weekend = {
     0: {
         "time": "10:00",
         "cost": middle_cost,
-        "order": 3
+        "order": 0
     },
     1: {
-        "time": "12:00",
+        "time": "11:30",
+        "cost": middle_cost,
+        "order": 1
+    },
+    2: {
+        "time": "13:00",
+        "cost": middle_cost,
+        "order": 2
+    },
+    3: {
+        "time": "14:30",
+        "cost": high_cost,
+        "order": 3
+    },
+    4: {
+        "time": "16:00",
         "cost": high_cost,
         "order": 4
     },
-    2: {
-        "time": "14:00",
+    5: {
+        "time": "17:30",
         "cost": high_cost,
         "order": 5
     },
-    3: {
-        "time": "16:00",
+    6: {
+        "time": "19:00",
         "cost": high_cost,
         "order": 6
     },
-    4: {
-        "time": "18:00",
+    7: {
+        "time": "20:30",
         "cost": high_cost,
         "order": 7
     },
-    5: {
-        "time": "20:00",
-        "cost": high_cost,
-        "order": 7
-    },
-    6: {
+    8: {
         "time": "22:00",
         "cost": high_cost,
-        "order": 7
+        "order": 8
     }
 }
